2412add3513d18756161f303b9ac4e513fa530c5
masooma babar <masoomababar216@gmail.com>
2023-08-06 20:33:43 +0500

    starting

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = ()=>{
+  return(
+    <> <h1>hello world</h1>
+    <span>masooma </span>
+   <img src="https://images.unsplash.com/photo-1526547541286-73a7aaa08f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" alt='' />
+    <p>this is good</p>
+    </>
+  );
+};
+export default App;
